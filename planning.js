3fb9ba7ff0b30d7661b4c20d9ab3d9ab2b563c91
keidy9 <keidyw999@gmail.com>
2023-10-26 23:18:58 -0700

    pre-planning

--- planning.js
+++ planning.js
@@ -1,3 +1,7 @@
+// have tiers of players from 1-3, tier 1 is 10-20 points, tier 2 is 13-25 points, tier 3 is 15-30 points.
+// do math.random() * the range of points + the lower cap of points (ex. for tier 2 25-13 = 12 -----> (math.random() * 12) + 13) 
+// calculate odds by taking average tier of players, calculated average points, then divide by another team.
+// calculate parlay odds by multiplying the chances plus adding some other multiply factor to incentivise parlays. 
 const team1 = {
     a: 50,
     b: 51,
@@ -12,7 +16,7 @@ const team2 = {
     b: 11,
     c: 80,
     d: 51,
-    e: 76,    
+    e: 76,
 }
 // total 278
 
